4c541f2b9ef211eb5a00fc3b80d789b6eac372db
zeb32ra <zeb32ra@gmail.com>
2022-11-22 21:42:51 +0300

    very last version

--- Work With Files.cs
+++ Work With Files.cs
@@ -7,7 +7,6 @@ namespace Files
 {
     internal class Work_With_Files
     {
-        /*string text = "17\nNastya\nVirgo\nProgrammer";*/
         People Nastya = new People("17", "Nastya", "Virgo", "Programmer");
         public string[] content;
         public List<People> result = new List<People>();
